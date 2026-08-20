a32f8c23f81700a915be072cc460510443b2e4cd
shubham-Alhat <shubhamalhat520@gmail.com>
2026-08-20 21:05:48 +0530

    starting from scratch

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,10 @@
 "use client";
 
-import { InputFieldgroup } from "@/utils/input-dialog";
-
 export default function Home() {
   return (
     <>
       <div className="h-screen w-full flex justify-center items-center ">
-        <div className="w-[300px] h-auto">
-          <InputFieldgroup />
-        </div>
+        <div className="w-[300px] h-auto">hellow world</div>
       </div>
     </>
   );
--- utils/viewer-stream.tsx
+++
@@ -1,9 +0,0 @@
-export const BuyerViewr = ({ roomId }: { roomId: string }) => {
-  console.log(roomId);
-
-  return (
-    <>
-      <div>buyer view component</div>
-    </>
-  );
-};
