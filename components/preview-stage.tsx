118b71e27e240c64510b7e4f356bda3fb3f4cc20
shubham-Alhat <shubhamalhat520@gmail.com>
2026-09-09 23:57:44 +0530

    BACK TO WHERE EVRYTHING STARTS

--- components/preview-stage.tsx
+++ components/preview-stage.tsx
@@ -243,13 +243,13 @@ export default function PreviewStage({
   return (
     <>
       <div>
-        <header className="w-full h-[62px] justify-center items-center bg-black text-blue-200 hidden lg:flex">
+        <header className="w-full h-[62px] flex justify-center items-center bg-black text-blue-200 hidden lg:flex sticky top-0 z-50">
           <nav>WELCOME TO KICK</nav>
         </header>
-
-        <main className="h-dvh overflow-hidden lg:min-h-0 lg:h-[calc(100vh-62px)] bg-blue-700 flex flex-col">
-          <div className="bg-neutral-25 px-4 py-4 flex-1 flex flex-col min-h-0">
-            <div className="block lg:grid gap-4 lg:relative fixed inset-0 overflow-y-auto overscroll-none lg:overflow-y-auto h-full lg:h-[calc(100vh-62px)] lg:min-h-0 items-start grid-rows-1 [grid-template-areas:'shop_player_sidebar'] grid-cols-[minmax(230px,1fr)_minmax(500px,2fr)_minmax(250px,1fr)]">
+        {/* MAIN COMP */}
+        <main className="min-h-screen lg:min-h-0 lg:h-[calc(100vh-62px)] bg-blue-500">
+          <div className="bg-neutral-25 px-4 pb-4">
+            <div className="block lg:grid gap-4 fixed lg:relative inset-0 lg:py-4 overflow-y-scroll lg:overflow-y-auto h-screen lg:h-[calc(100vh-62px)] lg:min-h-0 items-start mb-4 grid-rows-1 [grid-template-areas:'shop_player_sidebar'] grid-cols-[minmax(230px,1fr)_minmax(500px,2fr)_minmax(250px,1fr)]">
               {/* streamer section - centered one */}
               <div className="flex min-w-0 size-full flex-col gap-2 [grid-area:player]">
                 <section className="relative w-full h-full min-h-0 max-h-screen lg:max-h-[calc(100vh-62px)] lg:rounded-2xl overflow-hidden bg-neutral-900 aspect-9/16">
