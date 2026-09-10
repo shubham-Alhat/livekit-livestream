c2f1f1d90322068c463d79dfbb7aeba5f6fbac3a
shubham-Alhat <shubhamalhat520@gmail.com>
2026-09-10 21:01:12 +0530

    fix: update main layout to use h-screen for consistent height

--- components/preview-stage.tsx
+++ components/preview-stage.tsx
@@ -247,7 +247,7 @@ export default function PreviewStage({
           <nav>WELCOME TO KICK</nav>
         </header>
         {/* MAIN COMP */}
-        <main className="min-h-screen overflow-hidden overscroll-none lg:min-h-0 lg:h-[calc(100vh-62px)] bg-blue-500">
+        <main className="h-screen overflow-hidden overscroll-none lg:min-h-0 lg:h-[calc(100vh-62px)] bg-blue-500">
           <div className="bg-neutral-25 px-4 pb-4">
             <div className="block lg:grid gap-4 fixed lg:relative inset-0 lg:py-4 overflow-y-scroll lg:overflow-y-auto h-svh lg:h-[calc(100vh-62px)] lg:min-h-0 items-start mb-4 grid-rows-1 [grid-template-areas:'shop_player_sidebar'] grid-cols-[minmax(230px,1fr)_minmax(500px,2fr)_minmax(250px,1fr)]">
               {/* streamer section - centered one */}
