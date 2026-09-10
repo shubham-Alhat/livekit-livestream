803bc4d270fbeea72025b8c8958514637cee2927
shubham-Alhat <shubhamalhat520@gmail.com>
2026-09-10 21:10:59 +0530

    fix: update main layout to use h-svh for improved height management

--- components/preview-stage.tsx
+++ components/preview-stage.tsx
@@ -247,9 +247,9 @@ export default function PreviewStage({
           <nav>WELCOME TO KICK</nav>
         </header>
         {/* MAIN COMP */}
-        <main className="h-screen overflow-hidden overscroll-none lg:min-h-0 lg:h-[calc(100vh-62px)] bg-blue-500">
-          <div className="bg-neutral-25 px-4 pb-4">
-            <div className="block lg:grid gap-4 fixed lg:relative inset-0 lg:py-4 overflow-y-scroll lg:overflow-y-auto h-svh lg:h-[calc(100vh-62px)] lg:min-h-0 items-start mb-4 grid-rows-1 [grid-template-areas:'shop_player_sidebar'] grid-cols-[minmax(230px,1fr)_minmax(500px,2fr)_minmax(250px,1fr)]">
+        <main className="h-svh overflow-hidden overscroll-none lg:min-h-0 lg:h-[calc(100vh-62px)] bg-blue-500">
+          <div className="bg-neutral-25 px-4 pb-4 min-h-0">
+            <div className="block lg:grid gap-4 fixed lg:relative inset-0 lg:py-4 overflow-y-auto lg:overflow-y-auto h-full lg:h-[calc(100vh-62px)] lg:min-h-0 items-start mb-4 grid-rows-1 [grid-template-areas:'shop_player_sidebar'] grid-cols-[minmax(230px,1fr)_minmax(500px,2fr)_minmax(250px,1fr)]">
               {/* streamer section - centered one */}
               <div className="flex min-w-0 size-full flex-col gap-2 [grid-area:player]">
                 <section className="relative w-full h-full min-h-0 max-h-svh lg:max-h-[calc(100vh-62px)] lg:rounded-2xl overflow-hidden bg-neutral-900 aspect-9/16">
